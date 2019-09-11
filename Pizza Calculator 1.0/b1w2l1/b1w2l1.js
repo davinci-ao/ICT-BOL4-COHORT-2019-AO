9a34e40c2a92b8e580bb91a59d729adb354734a5
Valentijn <99057635@mydavinci.nl>
2019-09-11 09:05:31 +0200

    Data leak gemaakt die in gegeven data weergeeft via doc.write

---
+++ Pizza Calculator 1.0/b1w2l1/b1w2l1.js
@@ -0,0 +1,9 @@
+alert('Welkom bij deze Pizza Calculator');
+var voornaam=prompt('Voer uw Voornaamin'); //Vraagt om de Voornaam
+var achternaam=prompt('Voer uw achternaam in'); //Vraagt om achternaam
+var leeftijd=prompt('Voer uw leeftijd in'); //Vraagt om leeftijd
+var adress=prompt('Voer uw adress in om te bezorgen!'); //Vraagt om adress
+
+
+
+document.write(voornaam+achternaam);
