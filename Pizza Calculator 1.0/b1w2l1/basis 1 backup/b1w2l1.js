8d3784e7027ea86da656e3c0a4526cd99b1207e2
Valentijn <99057635@mydavinci.nl>
2019-09-11 09:53:54 +0200

    klaar met opdrqacht 1-3

--- Pizza Calculator 1.0/b1w2l1/basis 1 backup/b1w2l1.js
+++ Pizza Calculator 1.0/b1w2l1/basis 1 backup/b1w2l1.js
@@ -1,10 +1,8 @@
-//Valentijn Slijeper
-//mbo applicatie- en mediaontwikkelaar niveau 4
-
 alert('Welkom bij deze Pizza Calculator');
 var voornaam=prompt('Voer uw Voornaam in'); //Vraagt om de Voornaam
 var achternaam=prompt('Voer uw Achternaam in'); //Vraagt om achternaam
 var leeftijd=prompt('Voer uw leeftijd in'); //Vraagt om leeftijd
+var adress=prompt('Voer uw adress in om te bezorgen!'); //Vraagt om adress
 
 
 
